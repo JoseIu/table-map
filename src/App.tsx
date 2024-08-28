dc32df6dd5ca94105fcf5655b50bdf47ff9d7cc1
Joselu <joseluissgz97@gmail.com>
2024-08-28 23:35:44 +0200

    feat: Add image mapping functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { useEffect, useRef, useState } from "react";
 import "./App.css";
-interface Drawing {
-	x: number;
-	y: number;
-	width: number;
-	height: number;
+import Mapper from "./Mapper";
+export interface Coords {
+	x1: number;
+	y1: number;
+	x2: number;
+	y2: number;
+}
+
+export interface ImageMap {
+	image: string;
+	coords: Coords[];
 }
 
 function App() {
@@ -14,7 +20,17 @@ function App() {
 	);
 	const [isDrawing, setIsDrawing] = useState(false);
 	const [coors, setCoors] = useState({ x: 0, y: 0 });
-	const [drawings, setDrawings] = useState<Drawing[]>([]);
+
+	const [imageMap, setImageMap] = useState<ImageMap>({
+		image: "",
+		coords: [],
+	});
+
+	const setImgMap = (image: string) =>
+		setImageMap((prev) => ({ ...prev, image }));
+
+	const setCoords = (coords: Coords) =>
+		setImageMap((prev) => ({ ...prev, coords: [...prev.coords, coords] }));
 
 	const canvaRef = useRef<HTMLCanvasElement | null>(null);
 
@@ -24,6 +40,8 @@ function App() {
 			const reader = new FileReader();
 			reader.onload = (e) => {
 				setFileContent(e.target?.result as string);
+				const url = e.target?.result as string;
+				setImgMap(url);
 			};
 			reader.readAsDataURL(tableImage);
 		}
@@ -50,11 +68,7 @@ function App() {
 
 		const width = canvaRef.current.width;
 		const height = canvaRef.current.height;
-
-		setDrawings((prevDrawings) => [
-			...prevDrawings,
-			{ x: coors.x, y: coors.y, width, height },
-		]);
+		console.log("ONDOWN", { x, y });
 
 		setCoors({ x, y });
 	};
@@ -67,18 +81,32 @@ function App() {
 
 		const width = currentX - coors.x;
 		const height = currentY - coors.y;
-
-		//clear the canvas
 		const ctx = canvasRef.getContext("2d");
 
 		ctx?.fillRect(coors.x, coors.y, width, height);
 	};
-	const onMouseUp = () => {
+
+	const onMouseUp = (event: React.MouseEvent<HTMLCanvasElement>) => {
+		if (!isDrawing || canvaRef.current == null) return;
+		const { x, y } = getMousePositionOnCanva(event);
+
+		const newCoords = {
+			x1: coors.x,
+			y1: coors.y,
+			x2: x,
+			y2: y,
+		};
+
+		setCoords(newCoords);
 		setIsDrawing(false);
+	};
+	const onFinish = () => {
+		localStorage.setItem("imageMap", JSON.stringify(imageMap));
 	};
 
 	useEffect(() => {
 		if (!fileContent || canvaRef.current == null) return;
+
 		const canvas = canvaRef.current;
 		const ctx = canvas?.getContext("2d");
 
@@ -92,13 +120,15 @@ function App() {
 			ctx?.drawImage(image, 0, 0, image.width, image.height);
 		};
 	}, [fileContent]);
-	console.log({ drawings });
+
 	return (
 		<section className="app">
 			<h1 className="app__title">Table tac</h1>
 			<label className="file" htmlFor="table">
 				Select a image table
 				<input type="file" name="table" onChange={handleFileChange} />
+			</label>
+			<div>
 				<canvas
 					className="canvas"
 					ref={canvaRef}
@@ -106,7 +136,10 @@ function App() {
 					onMouseMove={onMouseMove}
 					onMouseUp={onMouseUp}
 				/>
-			</label>
+			</div>
+
+			{fileContent && <button onClick={onFinish}>Finalizar</button>}
+			<Mapper />
 		</section>
 	);
 }
